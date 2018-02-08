522ff0f900a07436b5df0410a61ae511da425233
medied <medina.eag@gmail.com>
2018-02-08 17:07:27 -0500

    Add two tests for transfer() function

--- test/VoteToken.test.js
+++ test/VoteToken.test.js
@@ -1,3 +1,5 @@
+import assertRevert from './helpers/assertRevert';
+
 var VoteToken = artifacts.require('VoteToken');
 
 contract('VoteToken', function (accounts) {
@@ -11,6 +13,20 @@ contract('VoteToken', function (accounts) {
     let totalSupply = await token.totalSupply();
 
     assert.equal(totalSupply, 200000000000);
+  });
+
+  it('should return correct balances after transfer', async function () {
+    await token.transfer(accounts[1], 1000);
+    let balance0 = await token.balanceOf(accounts[0]);
+    assert.equal(balance0, 199999999000);
+
+    let balance1 = await token.balanceOf(accounts[1]);
+    assert.equal(balance1, 1000);
+  });
+
+  it('should throw an error when trying to transfer more than balance', async function () {
+    // let token = await StandardTokenMock.new(accounts[0], 100);
+    await assertRevert(token.transfer(accounts[1], 200000000001));
   });
   
 });
