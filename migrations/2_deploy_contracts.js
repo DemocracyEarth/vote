36a3361651cce71a426d89acbaf5457fc6c7746c
medied <medina.eag@gmail.com>
2018-02-27 18:19:40 -0500

    Reset migration file

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,5 @@
 const VoteCrowdsale = artifacts.require("./VoteCrowdsale.sol")
 
 module.exports = function(deployer, network, accounts) {
-  const startTime = web3.eth.getBlock(web3.eth.blockNumber).timestamp + 1 // one second in the future
-  const endTime = startTime + (86400 * 20) // 20 days
-  const rate = new web3.BigNumber(1000)
-  const wallet = accounts[0]
-
-  deployer.deploy(VoteCrowdsale, startTime, endTime, rate, wallet)
+  // deployer.deploy(VoteCrowdsale, startTime, endTime, rate, wallet)
 };
