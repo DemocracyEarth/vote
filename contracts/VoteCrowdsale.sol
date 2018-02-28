b83c5498e6619766666e4ac390825213858ab629
medied <medina.eag@gmail.com>
2018-02-28 18:24:36 -0500

    Refactor VoteCrowdsale and add its testing file

--- contracts/VoteCrowdsale.sol
+++ contracts/VoteCrowdsale.sol
@@ -3,14 +3,7 @@ import 'zeppelin-solidity/contracts/crowdsale/Crowdsale.sol';
 import './VoteToken.sol';
 
 contract VoteCrowdsale is Crowdsale {
-
-  function VoteCrowdsale(uint256 _startTime, uint256 _endTime, uint256 _rate, address _wallet) 
-    Crowdsale(_startTime, _endTime, _rate, _wallet) {          
+  function VoteCrowdsale(uint256 _rate, address _wallet, VoteToken _token)
+    Crowdsale(_rate, _wallet, _token) {
   }
-
-  // creates the token to be sold
-  function createTokenContract() internal returns (MintableToken) {
-    return new VoteToken();
-  }
-
 }
